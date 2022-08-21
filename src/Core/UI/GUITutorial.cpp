711785269ad604948e9fbef739d910024e98ea91
TheSunCat <thesuncat123@gmail.com>
2022-08-21 16:59:53 +0200

    Make project name configurable

--- src/Core/UI/GUITutorial.cpp
+++ src/Core/UI/GUITutorial.cpp
@@ -14,7 +14,7 @@ GUITutorial::GUITutorial() : GUILayer("Tutorial", false), m_tutorial("Tutorial",
         o.pushOverlay(o.layerPtrs["characterMaker"]);
         o.pushOverlay(o.layerPtrs["stats"]);
 
-        ((GUIStats*)o.layerPtrs["stats"])->setTimer(/* TODO 150 */900);
+        ((GUIStats*)o.layerPtrs["stats"])->setTimer(150900);
     }));
 }
 
